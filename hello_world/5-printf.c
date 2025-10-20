aa2053a0c9c1140dbb0e9102cbb8cdab7ce2d962
root <root@ip-10-42-234-29.ec2.internal>
2025-10-20 13:31:20 +0000

    Add new task 05

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print "with proper grammar,but the outcome is a piece of art,"
+ * Using printf
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
