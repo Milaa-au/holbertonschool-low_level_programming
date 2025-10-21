cccf65745639b8cb5fe21a0632200602956ed6d8
Milaa-au <mila.audu@icloud.com>
2025-10-21 12:42:43 +0000

    Modification de la task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-printf("is positive %d\n", n);
-if (n == 0)
-printf("is zero %d\n", n);
-if (n < 0)
-printf("is negative%d\n", n);
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
 
 return (0);
 }
