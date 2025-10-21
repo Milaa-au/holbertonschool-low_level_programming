120fab0b836b6f6cf7c59e25e6fec3b176820dab
Milaa-au <mila.audu@icloud.com>
2025-10-21 12:21:28 +0000

    Modification de la task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,12 @@ Int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-	  {
-	    printf("is positive %d\n", n);
-	  }
-        else if (n == 0)
-	  {
-	    printf("is zero %d\n", n);
-	  }
-	else (n < 0)
-	  {
-	    printf("is negative%d\n", n);
-	  }
+if (n > 0)
+printf("is positive %d\n", n);
+else if (n == 0)
+printf("is zero %d\n", n);
+else (n < 0)
+printf("is negative%d\n", n);
 
 return (0);
 }
