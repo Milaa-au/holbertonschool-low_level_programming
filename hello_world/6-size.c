9a0800d09e18f11f668366eb1f7c75d427efae30
Milaa-au <mila.audu@icloud.com>
2025-10-21 07:48:39 +0000

    .

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 /**
- * main: Entry point of the program
- *
- * Description: program print the size various C
+ * main - Entry point of the program
  *
  * Return: Always 0 (success)
  */
