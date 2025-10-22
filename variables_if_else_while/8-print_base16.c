1bc6df5c0f60dcf93e3a6c0408654791e5430878
root <root@ip-10-42-197-2.ec2.internal>
2025-10-22 10:02:23 +0000

    .

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ char letter;
 
 for (letter = '0' ; letter <= '9'; letter++)
 putchar(letter);
-for (letter ='a' ; letter <= 'f'; letter++)
+for (letter = 'a' ; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
