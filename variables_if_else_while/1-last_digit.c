a68731882297bcbf7d81b98494081a7a3b198dfd
root <root@ip-10-42-197-2.ec2.internal>
2025-10-22 07:23:11 +0000

    Add new task01

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	sn = n % 10
+	sn = n % 10;
 if (sn > 5)
   printf("Last digit of %d is %d and is greater than 5\n", n, sn)
 else if (sn == 0)
