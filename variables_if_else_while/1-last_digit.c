4cdbcec93e674689804775c00c1e1d3bc12b7d5c
root <root@ip-10-42-197-2.ec2.internal>
2025-10-22 07:56:00 +0000

    .

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,9 +18,9 @@ if (sn < 0)
 sn = -sn;
 if (sn > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, sn);
-else if (sn < 6 && sn != 0)
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, sn);
-else
+else if (sn == 0)
 printf("Last digit of %d is %d and is 0\n", n, sn);
+else
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, sn);
 return (0);
 }
