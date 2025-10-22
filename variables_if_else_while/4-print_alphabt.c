4abc222728f71a0d6304fc67fb71053f0fde7681
root <root@ip-10-42-197-2.ec2.internal>
2025-10-22 08:55:14 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+
+char lettermin;
+
+lettermin = 'a';
+
+while (lettermin <= 'z'; lettermin++)
+if (letter != 'e' && letter != 'q')
+putchar(lettermin);
+lettermin++;
+
+putchar('\n');
+
+return (0);
+}
