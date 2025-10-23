b2a9c9e2aeaa42cb269aa0c503967ea7354edfc9
root <root@ip-10-42-201-183.ec2.internal>
2025-10-23 12:40:27 +0000

    .

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  * Return: 0 (Success)
  */
 
-void print_alphabet(void)
+int print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a', letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
