711d692d2df777b37717c51651649bfc6fcb633e
root <root@ip-10-42-126-167.ec2.internal>
2025-10-24 07:22:29 +0000

    .

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is alphabetic character.
+ * @c: The character to check
+ * Return: 1 if c is a letter lowercase or uppercase, 0 otherwise
+ *
+ */
+
+int _isalpha(int c)
+{
+	for ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
