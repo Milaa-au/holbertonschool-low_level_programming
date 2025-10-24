9b28a85ed3c2a840f438fff03a9ad6d869dbe99c
root <root@ip-10-42-126-167.ec2.internal>
2025-10-24 08:26:09 +0000

    .

---
+++ functions_nested_loops/7-rpint_last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to extract the last digit from
+ *
+ * Return: The value of the last digit
+ */
+
+int print_last_digit(int n);
+{
+	n = n % 10;
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+
+}
+
+
+
