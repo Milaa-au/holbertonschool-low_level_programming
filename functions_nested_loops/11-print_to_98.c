13b4f0ad5092bcd680c8164529ca34a896e83db7
root <root@ip-10-42-126-167.ec2.internal>
2025-10-24 12:53:40 +0000

    .

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Compte jusqu'a 98
+ * @n: nom d'origine
+ *
+ * Return: rien(void)
+ *
+ */
+
+void print_to_98(int n);
+{
+	if (n <= 98)
+	{
+
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+		else
+		{
+			for (; n >= 98; n--)
+			{
+				printf("%d", n);
+				if (n != 98)
+				{
+					printf(", ");
+				}
+			}
+		}
+	}
+	printf("\n");
+}
