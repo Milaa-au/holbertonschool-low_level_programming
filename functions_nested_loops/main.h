89a196504e6b584a210e8831eacadb4fc73f9bc8
root <root@ip-10-42-126-167.ec2.internal>
2025-10-24 11:55:00 +0000

    .

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
-void times_table(void)Ã;
+void times_table(void);
 int print_alphabet(void);
 void print_alphabet_x10(void);
 #endif /* MAIN_H*/
