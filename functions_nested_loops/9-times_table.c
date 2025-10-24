c478835c465879e8d27bb2a5afdfd49b8bec56b6
root <root@ip-10-42-126-167.ec2.internal>
2025-10-24 11:51:14 +0000

    .

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include <sdtdio.h>
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting from 0
+ *
+ * Return: nothing (void)
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (c == 0)
+			{
+				_putchar(b + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (c < 10)
+				{
+					_putchar(' ');
+					_putchar(c + '0');
+				}	
+				else 
+				{
+					_putchar(c / 10 + '0');
+					_putchar(c % 10 + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
