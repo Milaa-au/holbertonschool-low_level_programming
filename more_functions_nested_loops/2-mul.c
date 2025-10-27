090810e704c0f03a2c266e8f36d4373c6d9d2dc7
root <root@ip-10-42-205-152.ec2.internal>
2025-10-27 09:55:56 +0000

    Add task 02

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Multiplie deux entiers entre eux
+ * @a: est le premier entier
+ * @b: est le deuxieme entier
+ *
+ * Return: les entiers multiplies
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
