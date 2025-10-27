213398dfb91605a911de5bbd4c43db1a7052ef96
root <root@ip-10-42-205-152.ec2.internal>
2025-10-27 10:28:46 +0000

    Add task 04

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,13 @@
 
 void print_most_numbers(void)
 {
-	int n;
+	char n;
 
 	for (n = '0'; n <= '9'; n++)
 	{
 		if (n != 2 && n != 4)
 		{
-			_putchar(n + '0');
+			_putchar(n);
 		}
 	}
 	_putchar('\n');
