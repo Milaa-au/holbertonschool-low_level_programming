3c68881d257976116a7dff3070a7c207329ff5bd
root <root@ip-10-42-205-152.ec2.internal>
2025-10-27 13:24:15 +0000

    Add task 06

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Affiche le nombre de tiret demande
+ * @n: nombre de tiret
+ *
+ * Return: rien (void)
+ *
+ */
+
+void print_line(int n)
+{
+	if (n != 0)
+	{
+		for (; n <= 0; n++)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			for (; n => 0; n--)
+			{
+				_putchar('_');
+			}
+		}
+	}
+	_putchar('\n');
+}
+
+
