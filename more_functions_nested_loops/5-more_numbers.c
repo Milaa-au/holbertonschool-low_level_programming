a3455247c5b8bd244dc71794cd2ed51814276794
root <root@ip-10-42-205-152.ec2.internal>
2025-10-27 10:37:41 +0000

    Add task 05

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * more_numbers - Affichier les nombres de 0 a 14 10x
+ * 
+ * Return: rien (void)
  */
 
 void more_numbers(void)
@@ -12,9 +12,9 @@ void more_numbers(void)
 	int x = 0;
 	char n;
 
-	for (x = 0; x <= 14; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (n = '0'; n <= '9'; n++)
+		for (n = '0'; n <= '14'; n++)
 		{
 			_putchar(n);
 		}
