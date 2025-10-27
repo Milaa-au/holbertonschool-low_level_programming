5ec54b48c3ae58363c2e29171050aa58a46a43b0
root <root@ip-10-42-205-152.ec2.internal>
2025-10-27 10:07:53 +0000

    Add task 03

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,15 @@
 #include "main.h"
 
 /**
+ * print_numbers - va afficher 0123456789
  *
- *
- *
+ * Return: rien (void)
  */
 
 void print_numbers(void)
 {
 	int n;
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		_putchar(n);
