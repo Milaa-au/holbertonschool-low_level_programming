449dc53a7955caecfef590be03a4881b93663910
root <root@ip-10-42-185-49.ec2.internal>
2025-10-28 08:56:20 +0000

    .

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * FizzBuzz - Verifie multiple 3-5 ou les deux
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int FizzBuzz(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 10 == 5 && a % 10 == 0))
+		{
+			printf("Buzz ");
+		}
+		else if (a % 10 == 5 || a % 10 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	return (0);
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
+int FizzBuzz(void);
 void print_square(int size);
 void print_most_numbers(void);
 void more_numbers(void);
