1d8809b04d857c0de06c5466b8a875157697511e
Milaa-au <mila.audu@icloud.com>
2025-10-29 19:56:40 +0000

    Ajout de la question 03.

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Fonction qui affiche une string
+ * @str: La string en question
+ *
+ * Return: rien (void)
+ */
+
+void _puts(char *str)
+{
+
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		printf("%c", str[n]);
+	}
+}
