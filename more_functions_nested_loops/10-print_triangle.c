dd240e9f3bec5834e91218f2ff15846ea59d43eb
Milaa-au <mila.audu@icloud.com>
2025-10-29 08:38:43 +0000

    .

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -20,13 +20,14 @@ void print_triangle(int size)
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 0; j < size - i; j++)
+		{
 
 			putchar(' ');
-
+		}
 		for (j = 0; j < i; j++)
-
-		_putchar('#');
-
+		{
+			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
