b8bb92286845511a9eb99684f5e9918d94897d76
Milaa-au <mila.audu@icloud.com>
2025-10-30 14:11:23 +0000

    Ajout de la question 07.

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -10,24 +10,24 @@
 void puts_half(char *str)
 {
 	int n;
-	int taille;
+	int taille = 0;
 	int div;
 
 	for (n = 0; str[n] != '\0'; n++)
 	{
 		taille++;
-		if (taille % 2 == 0)
-		{
-			div = taille / 2;
-		}
-		else
-		{
-			div = (taille - 1) / 2 + 1;
-		}
-		for (n = div; n < taille; n++)
-		{
-			_putchar(str[n]);
-		}
+	}
+	if (taille % 2 == 0)
+	{
+		div = taille / 2;
+	}
+	else
+	{
+		div = (taille - 1) / 2 + 1;
+	}
+	for (n = div; n < taille; n++)
+	{
+		_putchar(str[n]);
 	}
 	_putchar('\n');
 }
