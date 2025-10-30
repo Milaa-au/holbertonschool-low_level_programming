7768297ce0fac2a8e21639353f09590a31c53dfb
Milaa-au <mila.audu@icloud.com>
2025-10-30 11:58:49 +0000

    Ajout de la question 05.

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -15,12 +15,11 @@ void rev_string(char *s)
 	char l;
 
 	for (n = 0; s[n] != '\0'; n++)
+	;
+	for (i = 0; i < n / 2; i++)
 	{
-		for (i = 0; i < n /2; i++)
-		{
-			l = s[i];
-			s[i] = s[n - 1 - i];
-		}
-			s[n - 1 - i] = l;
+		l = s[i];
+		s[i] = s[n - 1 - i];
+		s[n - 1 - i] = l;
 	}
 }
