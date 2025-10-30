c50bbef82cf3d6818f02890dc85c522758949f43
Milaa-au <mila.audu@icloud.com>
2025-10-30 10:47:58 +0000

    Ajout de la question 05.

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - Fonction qui inverse une string
+ * @s: string a inverser
+ *
+ * Return: rien (void)
+ */
+
+void rev_string(char *s)
+{
+	int n = 0;
+	int i, j;
+
+	while (s[n] != '\0')
+	{
+		_putchar(s[n]);
+		n++;
+	}
+	_putchar('\n');
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,4 +5,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _putchar(char c);
 void print_rev(char *s);
+void rev_string(char *s);
+
 #endif
