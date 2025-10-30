8d80480c28dd886176ead04d5c6291301680e2d3
Milaa-au <mila.audu@icloud.com>
2025-10-30 18:10:31 +0000

    SAVE

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <main.h>
+
+/**
+ * *_strcpy - Fonction qui copie la chaine de char pointer
+ * @dest: destination
+ * @src: pointer
+ *
+ * Ruturn: 0 (Success)
+ */
+
+void *_strcpy(char *dest, char *src)
+{
+	for 
