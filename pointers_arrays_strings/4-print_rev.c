b3a0333b5acd248d964f65de0bc983fda8b62fa9
Milaa-au <mila.audu@icloud.com>
2025-10-30 09:04:19 +0000

    Ajout de la question 04.

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Fonction qui renvoie l'interieur d'une string a l'envers
+ * @s: la string
+ *
+ * Return: rien (void)
+ */
+
+void print_rev(char *s)
+{
+	int n;
+
+	while (*s != '\0')
+	{
+		n++;
+	}
+	for (s[n] != '\0'; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _putchar(char c);
+void print_rev(char *s);
 #endif
