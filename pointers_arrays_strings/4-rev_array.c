ff8ab37bf6630da98b77427fd7f24dab79e833f0
Milaa-au <mila.audu@icloud.com>
2025-10-31 12:00:10 +0000

    Ajout de la question 04.

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - Fonction qui enverse chaine int
+ * @a: chaine int
+ * @n: int
+ *
+ * Return: rien (void)
+ */
+
+void reverse_array(int *a, int n)
+{
+	int e = n - 1;
+	int t;
+
+	for (; e >= 0; e--)
+	{
+		printf("%d", a[e]);
+		if (e != 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
