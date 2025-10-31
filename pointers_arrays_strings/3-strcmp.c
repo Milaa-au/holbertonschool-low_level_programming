bf89e68ae0b0dbbadecac63aad6e85a5b092569d
Milaa-au <mila.audu@icloud.com>
2025-10-31 11:43:13 +0000

    Ajout de la question 03.

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strcmp - Compare deux chaine de chars
+* @s1: 1er chaine de chars
+* @s2: 2e chaine de char
+* Return: fin 
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+
+	while (s1[x] != '\0' && s2[x] != '\0')
+	{
+		if (s1[x] != s2[x])
+		return (s1[x] - s2[x]);
+		x++;
+	}
+	return (s1[x] - s2[x]);
+}
