9254cf0dcd8857189365b2dccf4222db9f4dd9da
Milaa-au <mila.audu@icloud.com>
2025-10-31 16:18:52 +0000

    Ajout de la question 07.

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * leet - Fonction qui encore une chaine de char en 1337
+ * @str: Chaine de char
+ *
+ * Return: result (Sucess)
+ */
+
+char *leet(char *str)
+{
+	char l[] = "aAeEoOtTIL";
+	char n[] = "4433007711";
+	int x, i;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (str[x] == l[i])
+			{
+				str[x] = n[i];
+			break;
+			}
+		}
+	}
+	return (str);
+}
