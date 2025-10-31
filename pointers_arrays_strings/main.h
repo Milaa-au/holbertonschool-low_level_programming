96ed7a126803e9bbddee3e9eac9ae2931d0fd7c7
Milaa-au <mila.audu@icloud.com>
2025-10-31 11:16:44 +0000

    Ajout de la question 01.

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ void rev_string(char *s);
 void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 void print_array(int *a, int n);
 #endif
