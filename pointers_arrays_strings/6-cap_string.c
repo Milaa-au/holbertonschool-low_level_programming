fe74160d6eac52d2c72060135b15efa1e90aec8a
Milaa-au <mila.audu@icloud.com>
2025-10-31 13:01:07 +0000

    Ajout de la question 06.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string - Transforme la premier lettre en maj
+ * @str: tab char
+ *
+ * Return: char (SUCCESS)
+ */
+
+char *cap_string(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (str[a] >= 'a' && str[a] <= 'z' && str[a] == ' ')
+		{
+			str[a] = str[a] - 32;
+		}
+		a++;
+	}
+}
