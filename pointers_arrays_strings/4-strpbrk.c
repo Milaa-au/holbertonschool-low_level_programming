eccec0df573504856756ca84bb737228f9140301
Milaa-au <mila.audu@icloud.com>
2025-11-04 09:22:38 +0000

    Ajout de la question 04.

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strpbrk - Fonction qui cherche des char dans une chaine de char
+ * @s: La ou je vais chercher
+ * @accept: Ce qu'on cherche
+ *
+ * Return: accept or NULL (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return NULL;
+}
+
