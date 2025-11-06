0ba559d478ccbacbe9c35d728c6659c82143ed6f
Milaa-au <mila.audu@icloud.com>
2025-11-06 12:47:46 +0000

    Ajout de la question 00.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - programme qui affiche le nom
+ * @agrc: 1 er
+ * @agrv: renvoie le nom
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
