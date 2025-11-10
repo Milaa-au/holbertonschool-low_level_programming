a6fa651a8174dc9be56d9385700d96f736ab7c2a
Milaa-au <mila.audu@icloud.com>
2025-11-10 10:28:05 +0000

    SAVE

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdlib>
+#include <stdio.h>
+
+/**
+ * *_strdup - Fonction renvoie un pointer vers un espace memoire
+ * @str: pointer
+ *
+ * Return: Null if str = NULL
+ *
