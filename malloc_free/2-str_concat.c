a208786afcd68f4b08cd8385db289bbafe3bc21e
Milaa-au <mila.audu@icloud.com>
2025-11-12 08:34:09 +0000

    SAVE BUT ERROR

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *_strdup - Fonction qui concate 2 chaines de char.
+ * @s1: premier v
+ * @s2: deuxieme v
+ *
+ * Return: Pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int size, a, j, b, i;
+	char *result;
+	if (s1 == 0)
+	{
+		s1 = "";
+	}
+	if (s2 == 0)
+	{
+		s2 = "";
+	}
+
+	a = _strlen(s1);
+	b = _strlen(s2);
+	size = a + b + 1;
+
+	result = malloc(size);
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < a; i++)
+	{
+		result[i] = s1[i];
+	}
+	for (j = 0; j < b; j++)
+	{
+		result[i + j] = s2[j];
+	}
+	result[a + b] = '\0';
+	return (result);
+}
