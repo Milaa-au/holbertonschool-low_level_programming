fe7e8b2ffcc5e9a99b49b8c06f9a2b76ff574b88
Milaa-au <mila.audu@icloud.com>
2025-11-13 14:44:42 +0000

    Ajout de la question 00.

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Nouveau type de structure
+ * @name: nom du chien
+ * @age: age du chien
+ * @owner: le proprio du chien
+ */
+
+struct dog 
+{
+	char * name;
+	float age;
+	char * owner;
+}
+
+#endif
