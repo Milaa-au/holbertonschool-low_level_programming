cfc200f5b09aa6e0c4d45cf67c05865e72daba37
Milaa-au <mila.audu@icloud.com>
2025-11-14 11:39:52 +0000

    Ajout de la question 02.

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - Fonction qui affiche dog
+ * @d: dog
+ *
+ * Return: rien (void)
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
