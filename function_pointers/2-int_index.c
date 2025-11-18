537abc974d738b23a6d9bf532a0e4d011617f805
Milaa-au <mila.audu@icloud.com>
2025-11-18 09:15:12 +0000

    Ajout de la question 02 et modification de la 01 pour betty.

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * int_index - Fonction qui cherche un entier
+ * @array: tab de int
+ * @size: taille du tab
+ * @cmp: function pointeur
+ *
+ * Return: -1 if null or 0 ok
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
