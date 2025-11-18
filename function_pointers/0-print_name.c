f7ba7f837b56e86ea3d4ec8f5abf1921ca384177
Milaa-au <mila.audu@icloud.com>
2025-11-18 08:41:11 +0000

    Ajout de la question 00.

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Fonction qui s'appelle elle meme
+ * @name: nom
+ * @f: pointer sur fonction qui prend un char *
+ *
+ * Return: rien (void)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
