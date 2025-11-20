d449b50c62700b070fd034fbb8c17ec7063b9f3b
Milaa-au <Mila.audu@icloud.com>
2025-11-20 11:01:16 +0000

    Ajout de la question 01.

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - Affiche des nb suivis d'un saut à la ligne
+* @separator: .
+* @n: .
+*
+* Return: rien (void)
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int num;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+		if (separator != NULL && i < n - 1)
+		printf("%s", separator);
+	}
+
+	va_end(args);
+	printf("\n");
+}
