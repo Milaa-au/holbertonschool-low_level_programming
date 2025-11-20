0c8be2c5664a5dc9b95ce4bbca22e1aab4f2f041
Milaa-au <Mila.audu@icloud.com>
2025-11-20 10:48:13 +0000

    Ajout de la question 00.

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - Fonction qui calcul la somme des param
+* @n: .
+*
+* Return: entier (int)
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int valeur = va_arg(args, int);
+		sum += valeur;
+	}
+
+	va_end(args);
+	return (sum);
+}
