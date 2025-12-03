960766c07523370001c71a3fd302545803b7c776
Milaa-au <mila.audu@icloud.com>
2025-12-03 08:45:10 +0000

    Ajout d'un script qui libere une liste.

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* free_list - Fonction qui libère la liste
+* @head: Pointeur vers le premier élément de la liste
+*
+* Return: rien (void)
+*/
+
+void free_list(list_t *head)
+{
+    list_t *temp;
+
+    while (head != NULL)
+    {
+        temp = head->next;
+        free(head->str);
+        free(head);
+        head = temp;
+    }
+}
