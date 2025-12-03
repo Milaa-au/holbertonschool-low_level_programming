7ce385ea425915156020dc64e58adac4f2df5e43
Milaa-au <mila.audu@icloud.com>
2025-12-03 08:31:39 +0000

    Ajout d'un script qui ajoute les nouveaux noeuds à la fin

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* add_node - Fonction qui ajoute le noeud au dessus (et l'affiche).
+* @head: h
+* @str: d
+*
+* Return: new
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new;
+    list_t *temp;
+    unsigned int len = 0;
+
+    while (str[len])
+        len++;
+
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->str = strdup(str);
+    if (new->str == NULL)
+    {
+        free(new);
+        return (NULL);
+    }
+
+    new->len = len;
+    new->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new;
+    }
+    else
+    {
+        temp = *head;
+        while (temp->next != NULL)
+            temp = temp->next;
+
+        temp->next = new;
+    }
+
+    return (new);
+}
