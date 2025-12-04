efcc81b980d5cb91eccf14bd93e7f1a9dd4d5887
Milaa-au <Mila.audu@icloud.com>
2025-12-04 10:45:09 +0000

    Modification des commentaires Betty.

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -7,29 +7,32 @@
 /**
 * add_dnodeint - Fonction qui ajoute le noeud au dessus (et l'affiche).
 * @head: Pointeur vers le premier élément de la liste
-* @str: d
+* @n: d
 *
 * Return: new
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new;
+	dlistint_t *new;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
+	if (*head != NULL)
+	{
+		(*head)->prev = new;
+	}
+	*head = new;
 
-    new->n = n;
-    new->prev = NULL;
-    new->next = *head;
-
-    if (*head != NULL)
-        (*head)->prev = new;
-
-    *head = new;
-
-    return (new);
+	return (new);
 }
