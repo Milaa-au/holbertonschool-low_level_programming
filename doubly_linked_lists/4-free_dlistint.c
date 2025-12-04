44ef166d28869d4724186e7a55b39652c21120ae
Milaa-au <mila.audu@icloud.comc>
2025-12-04 14:46:07 +0000

    Modification et commentaire betty.

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -5,21 +5,22 @@
 #include <stdlib.h>
 
 /**
-* free_list - Fonction qui libère la liste
-* @head: Pointeur vers le premier élément de la liste
-*
-* Return: rien (void)
-*/
+ * free_dlistint - Libère tous les nœuds d'une liste doublement chaînée
+ * @head: Pointeur vers le premier nœud de la liste
+ *
+ * Description: Parcourt la liste doublement chaînée et libère
+ * la mémoire allouée pour chaque nœud.
+ */
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
+	dlistint_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head->next;
-        free(head->next);
-        free(head);
-        head = temp;
-    }
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->next);
+		free(head);
+		head = temp;
+	}
 }
--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -5,25 +5,32 @@
 #include <stdlib.h>
 
 /**
-* free_list - Fonction qui libère la liste
-* @head: Pointeur vers le premier élément de la liste
-*
-* Return: rien (void)
-*/
+ * get_dnodeint_at_index - Renvoie le nœud à un index donné
+ * d'une liste doublement chaînée
+ * @head: Pointeur vers le premier nœud de la liste
+ * @index: Index du nœud à récupérer (commence à 0)
+ *
+ * Description: Parcourt la liste doublement chaînée depuis le
+ * début et renvoie un pointeur vers le nœud situé à l'index
+ * spécifié. Si l'index est hors de portée, retourne NULL.
+ *
+ * Return: Pointeur vers le nœud à l'index donné, ou NULL si
+ * l'index n'existe pas
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *h = head;
-    unsigned int count = 0;
+	dlistint_t *h = head;
+	unsigned int count = 0;
 
-    while (h != NULL)
-    {
-        if (count == index)
-        {
-            return h;  /* Retourne le nœud à l'index demandé */
-        }
-        h = h->next;  /* Passe au nœud suivant */
-        count++;
-    }
-    return (NULL);
+	while (h != NULL)
+	{
+		if (count == index)
+		{
+			return (h);
+		}
+		h = h->next;
+		count++;
+	}
+	return (NULL);
 }
