f261861cc2ae61a05a21491646901e282193aea4
Milaa-au <mila.audu@icloud.comc>
2025-12-04 14:37:28 +0000

    Modification et ajout des commentaires pour Betty.

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,30 +13,30 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    unsigned int len = 0;
+	list_t *new;
+	unsigned int len = 0;
 
-    while (str[len])
+	while (str[len])
 	{
-        len++;
+		len++;
 	}
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-        return (NULL);
+		return (NULL);
 	}
 
-    new->str = strdup(str); /* strdup permet de alloue un nouvel espace mémoire*/
-    if (new->str == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
+	new->str = strdup(str); /* strdup permet de alloue un nouvel espace mémoire*/
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    new->len = len;
-    new->next = *head;
-    *head = new;
+	new->len = len;
+	new->next = *head;
+	*head = new;
 
-    return (new);
+	return (new);
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,39 +13,39 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new;
-    list_t *temp;
-    unsigned int len = 0;
+	list_t *new;
+	list_t *temp;
+	unsigned int len = 0;
 
-    while (str[len])
-        len++;
+	while (str[len])
+	{
+		len++;
+	}
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+	new->len = len;
+	new->next = NULL;
 
-    new->str = strdup(str);
-    if (new->str == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
-
-    new->len = len;
-    new->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        temp = *head;
-        while (temp->next != NULL)
-            temp = temp->next;
-
-        temp->next = new;
-    }
-
-    return (new);
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		temp = temp->next;
+		temp->next = new;
+	}
+	return (new);
 }
