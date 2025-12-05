3b087bde70ac4ec7834120fc151c033db7dd8959
Milaa-au <mila.audu@icloud.com>
2025-12-05 16:29:57 +0000

    Correction erreur betty git push

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -58,31 +58,21 @@ int main(int argc, char *argv[])
 	char *buffer;
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
-
 	buffer = create_buffer();
-
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-	{
 		dprintf(STDERR_FILENO,
 		"Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
 		exit(98);
-	}
-
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to == -1)
-	{
 		dprintf(STDERR_FILENO,
 		"Error: Can't write to %s\n", argv[2]);
 		free(buffer);
 		exit(99);
-	}
-
 	while ((r = read(from, buffer, 1024)) > 0)
 	{
 		w = write(to, buffer, r);
@@ -94,18 +84,13 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (r == -1)
-	{
 		dprintf(STDERR_FILENO,
 		"Error: Can't read from file %s\n", argv[1]);
 		free(buffer);
 		exit(98);
-	}
-
 	free(buffer);
 	close_file(from);
 	close_file(to);
-
 	return (0);
 }
