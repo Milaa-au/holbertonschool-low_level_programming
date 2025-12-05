860cd204342ad63999f85e899449fbcb828cb2e3
Milaa-au <mila.audu@icloud.com>
2025-12-05 14:22:52 +0000

    Ajout d'un script qui creer un fichier + des descriptions betty avec details pour les fichiers afin de mieux comprendre.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * read_textfile - Lit un fichier texte et l'affiche sur la sortie standard
+ * @filename: Nom du fichier à lire
+ * @letters: Nombre maximum de caractères à lire et à afficher
+ *
+ * Return: Le nombre réel de caractères lus et affichés,
+ *         ou 0 en cas d'erreur (fichier inexistant, lecture impossible,
+ *         écriture impossible ou filename NULL)
+ *
+ * Description:
+ * Cette fonction ouvre un fichier en lecture seule, lit jusqu'à un nombre
+ * spécifié de caractères et les écrit sur la sortie standard. Elle gère
+ * toutes les erreurs possibles : ouverture, allocation mémoire, lecture
+ * et écriture. En cas de problème, la fonction retourne 0.
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
---
+++ file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_file - Crée un fichier et y écrit du texte
+ * @filename: Nom du fichier à créer
+ * @text_content: Chaîne de caractères à écrire dans le fichier
+ *
+ * Return: 1 en cas de succès, -1 en cas d’échec
+ *
+ * Description:
+ * Cette fonction crée un fichier avec les permissions rw------- (0600).
+ * Si le fichier existe déjà, il est tronqué. Si text_content n'est pas NULL,
+ * son contenu est écrit dans le fichier. En cas d'erreur (fichier impossible
+ * à créer, ou écriture échouée), la fonction retourne -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (o == -1)
+	{
+		return (-1);
+	}
+
+	w = write(o, text_content, len);
+	if (w == -1)
+	{
+		return (-1);
+	}
+
+	close(o);
+	return (1);
+}
