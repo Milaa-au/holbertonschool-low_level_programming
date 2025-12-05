b924eabbb68f15d00381cb10162039ac2704a2d9
Milaa-au <mila.audu@icloud.com>
2025-12-05 16:39:48 +0000

    Changement pour adaptation a 40 lignes

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,94 +3,72 @@
 #include <stdio.h>
 
 /**
- * create_buffer - Alloue un tampon de 1024 octets.
- * @file: Nom du fichier associé au tampon
- * (utilisé pour les messages d’erreur).
+ * error_handler - Affiche un message d'erreur et quitte le programme.
+ * @exit_code: Code de sortie.
+ * @message: Message d'erreur (format printf).
+ * @type: Type de donnée attendu ('s', 'd' ou 'N').
  *
- * Return: Un pointeur vers le tampon alloué.
- *
+ * Description: Gère l'affichage des erreurs selon le type indiqué.
+ * Termine toujours le programme avec le code de sortie fourni.
  */
-
-char *create_buffer(void)
+void error_handler(int exit_code, char *message, char type, ...)
 {
-	char *buffer = malloc(sizeof(char) * 1024);
+	va_list args;
 
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't allocate buffer\n");
-		exit(99);
-	}
-	return (buffer);
-}
+	va_start(args, type);
 
-/**
- * close_file - Ferme un descripteur de fichier.
- * @fd: Le descripteur de fichier à fermer.
- */
+	if (type == 's')
+		dprintf(STDERR_FILENO, message, va_arg(args, char *));
+	else if (type == 'd')
+		dprintf(STDERR_FILENO, message, va_arg(args, int));
+	else if (type == 'N')
+		dprintf(STDERR_FILENO, "%s", message);
+	else
+		dprintf(STDERR_FILENO, "Error: Invalid type\n");
 
-void close_file(int fd)
-{
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+	va_end(args);
+	exit(exit_code);
 }
 
 /**
  * main - Copie le contenu d’un fichier dans un autre.
- * @argc: Nombre d’arguments transmis au programme.
- * @argv: Tableau contenant les arguments.
+ * @argc: Nombre d'arguments.
+ * @argv: Tableau d'arguments.
  *
- * Return: 0 en cas de succès, ou un code d’erreur en cas d’échec.
- *
- * Description:
- * Ce programme copie le contenu du fichier `file_from` vers `file_to`.
- * - Si le nombre d’arguments est incorrect → code 97.
- * - Si `file_from` n’existe pas ou ne peut être lu → code 98.
- * - Si `file_to` ne peut pas être créé ou écrit → code 99.
- * - Si un descripteur de fichier ne peut pas être fermé → code 100.
+ * Return: 0 en cas de succès.
  */
-
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
-	char *buffer;
+	char buffer[1024];
+	int fd_s, fd_d;
+	ssize_t r, w;
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	buffer = create_buffer();
-	from = open(argv[1], O_RDONLY);
-	if (from == -1)
-		dprintf(STDERR_FILENO,
-		"Error: Can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(98);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (to == -1)
-		dprintf(STDERR_FILENO,
-		"Error: Can't write to %s\n", argv[2]);
-		free(buffer);
-		exit(99);
-	while ((r = read(from, buffer, 1024)) > 0)
+		error_handler(97, "Usage: cp file_from file_to\n", 'N');
+
+	fd_s = open(argv[1], O_RDONLY);
+	if (fd_s == -1)
+		error_handler(98, "Error: Can't read from file %s\n", 's', argv[1]);
+
+	fd_d = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_d == -1)
+		error_handler(99, "Error: Can't write to %s\n", 's', argv[2]);
+
+	while ((r = read(fd_s, buffer, 1024)) > 0)
 	{
-		w = write(to, buffer, r);
+		w = write(fd_d, buffer, r);
 		if (w == -1)
-		{
-			dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
+			error_handler(99, "Error: Can't write to %s\n", 's', argv[2]);
 	}
+
 	if (r == -1)
-		dprintf(STDERR_FILENO,
-		"Error: Can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(98);
-	free(buffer);
-	close_file(from);
-	close_file(to);
+		error_handler(98, "Error: Can't read from file %s\n", 's', argv[1]);
+
+	if (close(fd_s) == -1)
+		error_handler(100, "Error: Can't close fd %d\n", 'd', fd_s);
+
+	if (close(fd_d) == -1)
+		error_handler(100, "Error: Can't close fd %d\n", 'd', fd_d);
+
 	return (0);
 }
