6101e8d3a7754817a01c89a4d57e00abd5e259fd
Milaa-au <mila.audu@icloud.com>
2025-12-05 12:51:40 +0000

    Ajout d'un script qui renvoie la somme des valeurs de chaque n.

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * sum_dlistint - Calcule la somme de tous les éléments (n) d'une liste dlistint_t
+ * @head: Pointeur vers le début de la liste doublement chaînée
+ *
+ * Return: La somme des valeurs de chaque n. Retourne 0 si la liste est vide.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
