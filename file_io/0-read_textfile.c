85aa0e7e5b7639fdc5c787bda0dbb14731362ae6
Milaa-au <mila.audu@icloud.com>
2025-12-05 13:56:25 +0000

    SAVE

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	o = open(filename, O_RDONLY);
+	if (o == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	r = read(o, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(o);
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
+	{
+		free(buffer);
+		close(o);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return(w);
+}
